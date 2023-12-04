06b1d65642c5dedbdd45796f0fb39f689558cf62
petit-pierre <aubree.pierre2@gmail.com>
2023-12-04 15:07:24 +0100

    V0.0.1 react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Slider from "./components/Slider";
+
+function App() {
+  return (
+    <div className="slider">
+      <Slider></Slider>
+    </div>
+  );
+}
+
+export default App;
