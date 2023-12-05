a4535ae71579460b8469811e3597962d994c36f8
petit-pierre <aubree.pierre2@gmail.com>
2023-12-05 12:13:49 +0100

    V0.0.6 test formulaire de contact

---
+++ smtp.js
@@ -0,0 +1,61 @@
+﻿/* SmtpJS.com - v3.0.0 */
+var Email = {
+  send: function (a) {
+    return new Promise(function (n, e) {
+      (a.nocache = Math.floor(1e6 * Math.random() + 1)), (a.Action = "Send");
+      var t = JSON.stringify(a);
+      Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) {
+        n(e);
+      });
+    });
+  },
+  ajaxPost: function (e, n, t) {
+    var a = Email.createCORSRequest("POST", e);
+    a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"),
+      (a.onload = function () {
+        var e = a.responseText;
+        null != t && t(e);
+      }),
+      a.send(n);
+  },
+  ajax: function (e, n) {
+    var t = Email.createCORSRequest("GET", e);
+    (t.onload = function () {
+      var e = t.responseText;
+      null != n && n(e);
+    }),
+      t.send();
+  },
+  createCORSRequest: function (e, n) {
+    var t = new XMLHttpRequest();
+    return (
+      "withCredentials" in t
+        ? t.open(e, n, !0)
+        : "undefined" != typeof XDomainRequest
+        ? (t = new XDomainRequest()).open(e, n)
+        : (t = null),
+      t
+    );
+  },
+};
+
+// mon code //
+send = document.querySelector(".send");
+send.addEventListener("click", (e) => {
+  e.preventDefault();
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "aubree.pierre2@gmail.com",
+    Password: "98CF43CAAA79FD01D7FB08752FD07AE78C4D",
+    To: "contact@petitpierre.net",
+    From: "aubree.pierre2@gmail.com",
+    Subject: "Site mariage-en-morgan",
+    Body:
+      "email : " +
+      document.querySelector(".email").value +
+      " tel : " +
+      document.querySelector(".tel").value +
+      " message : " +
+      document.querySelector(".message").value,
+  }).then((message) => alert(message));
+});
