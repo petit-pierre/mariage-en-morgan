2a4047465d1c993473b42424368e6ff6d1374b6d
petit-pierre <aubree.pierre2@gmail.com>
2024-07-24 17:32:28 +0200

    V1.4.3 manque la logique pour toutes les photos

--- conponents/connect.js
+++ conponents/connect.js
@@ -1,5 +1,15 @@
 const tokenn = window.localStorage.getItem("token");
 if (tokenn !== null) {
-  document.querySelector(".plume").classList.remove("hidden");
   document.querySelector(".logOut").classList.remove("hidden");
+  document.querySelector(".fullPic").classList.remove("hidden");
+  document.querySelector(".plume").classList.remove("hidden");
+
+  for (let i = 0; i < 13; i++) {
+    document.querySelector(".firstEdit" + i).classList.remove("hidden");
+  }
+
+  document.querySelector(".logOut").addEventListener("click", (evt) => {
+    window.localStorage.removeItem("token");
+    document.location.href = "./index.html";
+  });
 }
