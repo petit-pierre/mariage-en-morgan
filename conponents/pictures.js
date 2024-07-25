8c1c969af9bd57e775565ad1c03b4a3d3d02b473
petit-pierre <aubree.pierre2@gmail.com>
2024-07-25 10:59:41 +0200

    V1.5.0 job's done

--- conponents/pictures.js
+++ conponents/pictures.js
@@ -15,7 +15,7 @@ async function getItems() {
       //evt.explicitOriginalTarget.offsetParent.children[0].src
       obj.alt === "full"
   );
-  document.querySelector(".firstPic").setAttribute("src", fullPic.picture);
+
   for (let i = 0; i < result.length; i++) {
     if (result[i].alt === "first") {
       firstCollection.push(result[i]);
@@ -24,9 +24,17 @@ async function getItems() {
       secondCollection.push(result[i]);
     }
   }
+  console.log(firstCollection);
+
   firstCollection.sort(function (a, b) {
     return a.french_content - b.french_content;
   });
+
+  for (let i = 0; i < 14; i++) {
+    document
+      .querySelector(".photo" + i)
+      .setAttribute("src", firstCollection[i].picture);
+  }
 
   secondCollection.sort(function (a, b) {
     return a.french_content - b.french_content;
@@ -121,13 +129,84 @@ async function putFullPic(fullPic) {
 
 async function changePic(firstCollection, token) {
   let wichPic;
-  for (let i = 0; i < 13; i++) {
+  for (let i = 0; i < 14; i++) {
     document.querySelector(".btn" + i).addEventListener("click", (evt) => {
       wichPic = i;
       document.querySelector(".uploadFullPic").click();
     });
   }
   document.querySelector(".uploadFullPic").addEventListener("change", (evt) => {
-    console.log(wichPic);
+    let slide = firstCollection[wichPic];
+    console.log(slide);
+
+    let id = slide.picture_id;
+    let slideId = slide._id;
+    delettePic(id, token, slideId);
+
+    async function delettePic(id, token, slideId) {
+      const response = await fetch(
+        "https://api.petitpierre.net/api/pictures/" + id,
+        {
+          method: "DELETE",
+
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      let result = await response.json();
+      deletteSlide(slideId, token);
+    }
+
+    async function deletteSlide(slideId, token) {
+      const response = await fetch(
+        "https://api.petitpierre.net/api/sliders/" + slideId,
+        {
+          method: "DELETE",
+
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      let result = await response.json();
+    }
+
+    let slider = document.querySelector(".uploadFullPic").files[0];
+    const formData = new FormData();
+    formData.append("imageUrl", "");
+    formData.append("image", slider);
+
+    sendPic(formData, token);
+    async function sendPic(formData, token) {
+      const response = await fetch("https://api.petitpierre.net/api/pictures", {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+        body: formData,
+      });
+      let result = await response.json();
+
+      slide.picture = result.imageUrl;
+      slide.picture_id = result._id;
+
+      await sendItems(slide, token);
+    }
+    async function sendItems(slide, token) {
+      const response = await fetch("https://api.petitpierre.net/api/sliders", {
+        method: "POST",
+
+        headers: {
+          Authorization: "Bearer " + token,
+          Accept: "application/json",
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(slide),
+      });
+
+      let result = await response.json();
+      document.location.href = "./index.html";
+    }
   });
 }
